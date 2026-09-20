3dd73fd1cf92a74810c7fc78a23d96cfed361e78
Maicol garcia <maicolalexgarciainga@gmail.com>
2026-09-19 20:11:42 -0500

    Configurar puerto 10000 para Render

--- src/main/java/com/ejemplo/miprimerapagina/HolaController.java
+++ src/main/java/com/ejemplo/miprimerapagina/HolaController.java
@@ -1,5 +1,17 @@
 package com.ejemplo.miprimerapagina;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HolaController {
 
+    @GetMapping("/")
+    public String inicio() {
+        return """
+                <h1>Mi primera aplicacion Spring Boot</h1>
+                <h2>Aplicacion publicada en Internet</h2>
+                <p>Desarrollado con Java y Spring Boot.</p>
+                """;
+    }
 }
