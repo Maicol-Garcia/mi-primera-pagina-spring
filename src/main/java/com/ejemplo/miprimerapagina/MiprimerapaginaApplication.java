44a42922b25a79ebda4d7a61796a2cfaaa0037a4
Maicol garcia <maicolalexgarciainga@gmail.com>
2026-09-19 19:03:00 -0500

    Primera version de mi pagina Spring Boot

---
+++ src/main/java/com/ejemplo/miprimerapagina/MiprimerapaginaApplication.java
@@ -0,0 +1,13 @@
+package com.ejemplo.miprimerapagina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiprimerapaginaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiprimerapaginaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ejemplo/miprimerapagina/MiprimerapaginaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejemplo.miprimerapagina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiprimerapaginaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
